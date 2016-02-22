075de07e415f8f5eb941f56f7f296ce49beb35d6
kfarr <kieran.farr@gmail.com>
2016-02-21 21:50:50 -0800

    Initialize repo with work-in-progress files

---
+++ client/js/controllers/review.js
@@ -0,0 +1,110 @@
+angular
+  .module('app')
+  .controller('AllReviewsController', ['$scope', 'Review', function($scope,
+      Review) {
+    $scope.range = function(count){
+      var ratings = [];
+      for (var i = 0; i < count; i++) {
+        ratings.push(i)
+      }
+      return ratings;
+    };
+    $scope.reviews = Review.find({
+      filter: {
+        include: [
+          'coffeeShop',
+          'reviewer'
+        ]
+      }
+    });
+  }])
+  .controller('AddReviewController', ['$scope', 'CoffeeShop', 'Review',
+      '$state', function($scope, CoffeeShop, Review, $state) {
+    $scope.action = 'Add';
+    $scope.coffeeShops = [];
+    $scope.selectedShop;
+    $scope.review = {};
+    $scope.isDisabled = false;
+    $scope.date = Date();
+
+    CoffeeShop
+      .find()
+      .$promise
+      .then(function(coffeeShops) {
+        $scope.coffeeShops = coffeeShops;
+        $scope.selectedShop = $scope.selectedShop || coffeeShops[0];
+      });
+
+    $scope.submitForm = function() {
+      Review
+        .create({
+          rating: $scope.review.rating,
+          comments: $scope.review.comments,
+          coffeeShopId: $scope.selectedShop.id,
+          date: $scope.date
+        })
+        .$promise
+        .then(function() {
+          $state.go('all-reviews');
+        });
+    };
+  }])
+  .controller('DeleteReviewController', ['$scope', 'Review', '$state',
+      '$stateParams', function($scope, Review, $state, $stateParams) {
+    Review
+      .deleteById({ id: $stateParams.id })
+      .$promise
+      .then(function() {
+        $state.go('my-reviews');
+      });
+  }])
+  .controller('EditReviewController', ['$scope', '$q', 'CoffeeShop', 'Review',
+      '$stateParams', '$state', function($scope, $q, CoffeeShop, Review,
+      $stateParams, $state) {
+    $scope.action = 'Edit';
+    $scope.coffeeShops = [];
+    $scope.selectedShop;
+    $scope.review = {};
+    $scope.isDisabled = true;
+
+    $q
+      .all([
+        CoffeeShop.find().$promise,
+        Review.findById({ id: $stateParams.id }).$promise
+      ])
+      .then(function(data) {
+        var coffeeShops = $scope.coffeeShops = data[0];
+        $scope.review = data[1];
+        $scope.selectedShop;
+
+        var selectedShopIndex = coffeeShops
+          .map(function(coffeeShop) {
+            return coffeeShop.id;
+          })
+          .indexOf($scope.review.coffeeShopId);
+        $scope.selectedShop = coffeeShops[selectedShopIndex];
+      });
+
+    $scope.submitForm = function() {
+      $scope.review.coffeeShopId = $scope.selectedShop.id;
+      $scope.review
+        .$save()
+        .then(function(review) {
+          $state.go('all-reviews');
+        });
+    };
+  }])
+  .controller('MyReviewsController', ['$scope', 'Review', '$rootScope',
+      function($scope, Review, $rootScope) {
+    $scope.reviews = Review.find({
+      filter: {
+        where: {
+          publisherId: $rootScope.currentUser.id
+        },
+        include: [
+          'coffeeShop',
+          'reviewer'
+        ]
+      }
+    });
+  }]);
